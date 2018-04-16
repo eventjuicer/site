949fc71602b365f41c9556e8c55203b22ce730fb
Adam Zygadlewicz <adam@zygadlewicz.com>
2018-04-16 16:47:47 +0200

    bind default OG image with company data lang info

--- helpers/data.js
+++ helpers/data.js
@@ -4,6 +4,8 @@ import _filter from 'lodash/filter'
 import { getUrlParams } from './links'
 
 
+export const defaultLocale = "pl"
+
 export const getCompanyProfileInfo = (company, key) => _get(company, `profile.${key}`, "")
 
 
@@ -17,12 +19,12 @@ export const getCdnResource = (company, key, scale = true) => {
 }
 
 
-export const getPresenterOgImage = (participant) => {
+export const getPresenterOgImage = (participant, template = "template_speaker") => {
 
   const avatarTrans = `g_west,x_150,y_25,w_220,h_220,l_p_${participant.id}_avatar,c_fit,r_max`
   const logotypeTrans = `g_east,x_150,y_25,w_220,h_220,l_p_${participant.id}_logotype,c_fit`
 
-  return `http://res.cloudinary.com/eventjuicer/image/upload/${avatarTrans}/${logotypeTrans}/template_speaker.png`
+  return `http://res.cloudinary.com/eventjuicer/image/upload/${avatarTrans}/${logotypeTrans}/${template}.png`
 
 }
 
@@ -30,14 +32,17 @@ export const getCompanyAltOgImage = (company, url) => {
 
   const params = getUrlParams(url)
 
-  if("utm_content" in params && params.utm_content.indexOf("logotype") > -1){
+  //logotype is default....is we have something different we try to use it
+  if("utm_content" in params && params.utm_content.indexOf("logotype") === -1){
+
+    //clear parameters utm_content params?
     const cdn = getCdnResource(company, params.utm_content, false)
     if(cdn){
       return wrapImage(`c_${company.id}_${params.utm_content}`, "template_raw", "h_504,w_960")
     }
   }
 
-  return getCompanyOgImage(company)
+  return getCompanyOgImage(company, url)
 }
 
 export const getCompanyLogotype = (company, scale = true) => {
@@ -66,14 +71,12 @@ export const getCompanyOgImage = (company) => {
     return getCompanyLogotype(company, true)
   }
 
-  if(getCompanyProfileInfo("countries") !== "national")
-  {
-    return wrapImage(`c_${company.id}_logotype`, "template_4_en")
-  }
+  const companyLang = getCompanyProfileInfo(company, "lang") || defaultLocale
 
-  return wrapImage(`c_${company.id}_logotype`, "template_4_pl")
+  return wrapImage(`c_${company.id}_logotype`, `template_4_${companyLang}`)
 
 }
+
 
 
 export const filterCompanyInstances = (company, eventId) => _filter(company, function(i){
