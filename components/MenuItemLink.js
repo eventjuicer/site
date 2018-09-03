d6a71c2bb28b3d763342629dea5bea358ea287a4
Adam Zygadlewicz <adam@zygadlewicz.com>
2018-09-03 14:07:33 +0200

    scroll after route change

--- components/MenuItemLink.js
+++ components/MenuItemLink.js
@@ -12,7 +12,7 @@ import ListItemText from '@material-ui/core/ListItemText';
 import { translate } from '../i18n';
 
 const MenuItemLink = ({ to, baseLabel, label, text, icon, translate }) => (
-  <ListItem button={true} onClick={() => Router.push(to)}>
+  <ListItem button={true} onClick={() => Router.push(to).then(() => window.scrollTo(0, 0)) }>
     {/* {icon && <ListItemIcon>{icon}</ListItemIcon>} */}
     <ListItemText
       primary={label ? translate(`common.menu.${baseLabel}.${label}`) : text}
--- compositions/RoleButtons.js
+++ compositions/RoleButtons.js
@@ -7,13 +7,13 @@ const _items = [
       url: 'https://static.eventjuicer.com/photos/12961446_1288640741145929_7684227399478032531_o.jpg',
       label: 'common.visitor',
       width: '50%',
-      onClick : () => Router.push("/visit")
+      onClick : () => Router.push("/visit").then(() => window.scrollTo(0, 0))
     },
     {
       url: 'https://static.eventjuicer.com/photos/12967348_1288628734480463_3860331543127036065_o.jpg',
       label: 'common.exhibitor',
       width: '50%',
-      onClick : () => Router.push("/exhibit")
+      onClick : () => Router.push("/exhibit").then(() => window.scrollTo(0, 0))
     },
   
   ];
