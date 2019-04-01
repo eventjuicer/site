2da9a9550af2d03653a4a187035471f704e27aee
Adam Zygadlewicz <adam@zygadlewicz.com>
2019-04-02 00:21:25 +0200

    cached translations

--- server/i18n.js
+++ server/i18n.js
@@ -1,5 +1,15 @@
 const fetch = require('isomorphic-unfetch');
 const translationUrl = `https://localise.biz/api/export/all.json?format=multi&pretty&key=SHiwxgKaPMx_KThQH2zcdzwiKEMzuNBm`;
+const defaultTranslations = require("./public-bundle.json");
+
+
+function handleErrors(response) {
+  if (!response.ok) {
+      throw Error(response.statusText);
+  }
+  return response;
+}
+
 
 async function getTexts(cache, purge) {
   if (purge) {
@@ -7,7 +17,9 @@ async function getTexts(cache, purge) {
   }
 
   if (!cache.has(translationUrl)) {
-    const data = await fetch(translationUrl).then(response => response.json());
+    
+    const data = await fetch(translationUrl, {timeout : 2000}).then(handleErrors).then(response => response.json()).catch(error => defaultTranslations);
+
     cache.set(translationUrl, data);
    // console.log('API/texts not found! Fetching and caching...');
     return data;
@@ -18,3 +30,4 @@ async function getTexts(cache, purge) {
 }
 
 module.exports = { getTexts };
+
