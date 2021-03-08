bc4ff60014c6d786ca2319cb672fc0a5b58d45cc
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-03-08 01:57:19 +0100

    3.1.12

---
+++ pages/stages/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import {
+    connect,
+    reduxWrapper
+  } from 'eventjuicer-site-components';
+  
+export const getServerSideProps = reduxWrapper.getServerSideProps(async (props) => {
+  
+    return {
+        redirect: {
+          permanent: true,
+          destination: "/stages/a"
+        },
+        props: {}
+    }
+
+})
+
+export default connect()(Redirect);
+  
